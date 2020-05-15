83407c9c120978780a2de01686453fcfd6b41ad1
Saksham Dhull <saksham18186@iiitd.ac.in>
2020-05-15 16:01:18 +0530

    Basic

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,70 @@
+<template>
+	<div class="question-box-container">
+		<b-jumbotron>
+			<template v-slot:lead>
+				{{ currentQuestion.question }}
+			</template>
+			<hr class="my-4">
+			<b-list-group>
+				<b-list-group-item 
+					v-for="(answer,index) in answers" 
+					v-bind:key="index"
+					@click="selectAnswer(index)"
+					v-bind:class="[selectedIndex === index ? 'selected': '']"
+				>
+					{{answer}}
+				</b-list-group-item>
+			</b-list-group>
+			<b-button variant="primary" href="#">Submit</b-button>
+			<b-button @click="next" variant="success" href="#">Next</b-button>
+		</b-jumbotron>
+	</div>
+</template>
+<script>
+	export default{
+		props: {
+			currentQuestion: Object,
+			next: Function
+		},
+		data(){
+			return {
+				selectedIndex: null
+			}
+		},
+		computed:{
+			answers(){
+				let answers=[...this.currentQuestion.incorrect_answers]
+				answers.push(this.currentQuestion.correct_answer)
+				return answers
+			}
+		},
+		methods:{
+			selectAnswer(index)
+			{
+				this.selectedIndex=index
+				console.log(index)
+			}
+		}
+	}
+</script>
+<style scoped>
+	.list-group{
+		margin-bottom: 15px;
+	}
+	.list-group-item:hover{
+		background-color: #EEE;
+		cursor:pointer;
+	}
+	.btn{
+		margin:0px 5px 0px 5px;
+	}
+	.selected{
+		background-color: lightblue;
+	}
+	.correct{
+		background-color: lightgreen;
+	}
+	.incorrect{
+		background-color: red;
+	}
+</style>
